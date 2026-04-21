902c907130c9f7fc79a4628b6e732b06ea8df7cf
alejavierriveros <alej.riveros@duocuc.cl>
2026-04-20 20:07:40 -0400

    V1
    subiendo la primera version del catalogService

---
+++ libreriaMarketCatalogService/src/main/java/com/example/libreriaMarketCatalogService/LibreriaMarketCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.libreriaMarketCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaMarketCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaMarketCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ libreriaMarketCatalogService/src/test/java/com/example/libreriaMarketCatalogService/LibreriaMarketCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libreriaMarketCatalogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaMarketCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
