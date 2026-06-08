c45507f4e3607d9ec401cf29fe284634f1bcc4c1
Josue-Solar <josu.solar@duocuc.cl>
2026-06-08 16:10:13 -0400

    ApiResponses agregados a ambos controllers

--- libreriaMarketCatalogService/src/main/java/com/example/libreriaMarketCatalogService/controller/ProductoController.java
+++ libreriaMarketCatalogService/src/main/java/com/example/libreriaMarketCatalogService/controller/ProductoController.java
@@ -11,6 +11,11 @@ import com.example.libreriaMarketCatalogService.mappers.*;
 import com.example.libreriaMarketCatalogService.service.*;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -25,32 +30,137 @@ public class ProductoController {
         this.service = service;
     }
 
-    @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Productos encontrados",
+                content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = ProductoResponseDTO.class))
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se han encontrado productos",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Lista todos", description = "Muestra todos los registros de productos")
+    @GetMapping
     public List<ProductoResponseDTO> listar() {
         return service.listar().stream().map(ProductoMapper::toDto).toList();
     }
 
-    @GetMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Producto encontrado",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductoResponseDTO.class)
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado el producto",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Busca por ID", description = "Encuentra producto por ID")
+    @GetMapping("/{id}")
     public ProductoResponseDTO obtener(@PathVariable Long id) {
         return ProductoMapper.toDto(service.obtener(id));
     }
 
-    @GetMapping("/exists-by-id/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Sí existe",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductoResponseDTO.class)
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Valida existencia", description = "Verifica si el producto existe o no según su ID")
+    @GetMapping("/exists-by-id/{id}")
     public ResponseEntity<Boolean> existsById(@PathVariable Long id) {
         return ResponseEntity.ok(service.existsById(id));
     }
 
-    @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "201",
+                description = "Producto guardado",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductoResponseDTO.class)
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflicto al hacer solicitud",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Crea registro producto", description = "Guarda nuevo producto")
+    @PostMapping
     public ProductoResponseDTO crear(@Valid @RequestBody ProductoInputDTO dto) {
         return service.guardar(dto);
     }
 
-    @PutMapping("/{id}")
+     @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Producto actualizado",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductoResponseDTO.class)
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado producto",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Actualizar informacion", description = "Actualiza datos de producto encontrado por ID")
+    @PutMapping("/{id}")
     public ProductoResponseDTO actualizar(@PathVariable Long id, @Valid @RequestBody ProductoInputDTO dto) {
         return ProductoMapper.toDto(
                 service.actualizar(
@@ -61,8 +171,29 @@ public class ProductoController {
         );
     }
 
-    @DeleteMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                responseCode = "204",
+                description = "Producto eliminado",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ProductoResponseDTO.class)
+                )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado producto",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Eliminar registro", description = "Borra registro de producto")
+    @DeleteMapping("/{id}")
     public String eliminar(@PathVariable Long id) {
         service.eliminar(id);
         return "Producto eliminado";
--- libreriaMarketCatalogService/src/main/java/com/example/libreriaMarketCatalogService/controller/ProveedorController.java
+++ libreriaMarketCatalogService/src/main/java/com/example/libreriaMarketCatalogService/controller/ProveedorController.java
@@ -10,6 +10,11 @@ import com.example.libreriaMarketCatalogService.mappers.*;
 import com.example.libreriaMarketCatalogService.service.*;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -24,32 +29,137 @@ public class ProveedorController {
         this.service = service;
     }
 
-    @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se han encontrado registros",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ProveedorDTO.class))
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se han encontrado el registros",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Lista todos", description = "Muestra todos los registros de proveedores")
+    @GetMapping
     public List<ProveedorDTO> listar() {
         return service.listar().stream().map(ProveedorMapper::toDTO).toList();
     }
 
-    @GetMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se ha encontrado el registro",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProveedorDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado el registro de proveedor",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Busca por ID", description = "Encuentra proveedor por ID")
+    @GetMapping("/{id}")
     public ProveedorDTO obtener(@PathVariable Long id) {
         return ProveedorMapper.toDTO(service.obtener(id));
     }
 
-    @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Se ha guardado el registro",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProveedorDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflicto al guardar",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Crea registro proveedor", description = "Guarda nuevo proveedor")
+    @PostMapping
     public ProveedorDTO crear(@Valid @RequestBody ProveedorDTO dto) {
         return ProveedorMapper.toDTO(service.guardar(ProveedorMapper.toEntity(dto)));
     }
 
-    @PutMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se ha actualizado el registro",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProveedorDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se han encontrado registros",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Actualizar informacion", description = "Actualiza datos de proveedor encontrado por ID")
+    @PutMapping("/{id}")
     public ProveedorDTO actualizar(@PathVariable Long id, @Valid @RequestBody ProveedorDTO dto) {
         return ProveedorMapper.toDTO(service.actualizar(id, ProveedorMapper.toEntity(dto)));
     }
 
-    @DeleteMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Se ha eliminado el registro de proveedor",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProveedorDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Sintáxis incorrecta",
+                    content = @Content(schema = @Schema(hidden = true))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado registro de proveedor",
+                    content = @Content(schema = @Schema(hidden = true))
+            )
+        }
+    )
     @Operation(summary = "Eliminar registro", description = "Borra registro de proveedor")
+    @DeleteMapping("/{id}")
     public String eliminar(@PathVariable Long id) {
         service.eliminar(id);
         return "Proveedor eliminado";
